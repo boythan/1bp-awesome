8ab5bd1ed32cb6ca06bcc45cd732936c98b66ad0
Vinh-Mac <vinh@abivin.com>
2020-08-10 21:32:44 +0700

    change export module

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import styles from './styles.module.css'
 import TableAwesomeComponent from './components/table/TableAwesomeComponent'
 
-export const TableAwesomeComponent = TableAwesomeComponent
+export { TableAwesomeComponent }
