d0b4c425b43052e925d00891b3cd9ef93badae8c
Vinh-Mac <vinh@abivin.com>
2020-08-10 17:12:24 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '1bp-awesome'
+import '1bp-awesome/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
